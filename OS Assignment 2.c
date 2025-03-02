0ca7dcdba7dce74a4b72e4c872446b0dd312236c
Arjun Anand <anandarjun48@gmail.com>
2025-03-02 18:21:41 -0500

    Updated code

--- OS Assignment 2.c
+++ OS Assignment 2.c
@@ -3,82 +3,82 @@
 #include <pthread.h>
 #include <unistd.h>
 
-#define NUM_PHILOSOPHERS 5 // Number of the philosophers
-typedef enum { THINKING, HUNGRY, EATING } state_t; // State of the philosopher
+#define NUM_PHILOSOPHERS 5 // Number of philosophers
+typedef enum { THINKING, EATING } state_t; // States
 state_t state[NUM_PHILOSOPHERS];
 
-pthread_mutex_t mutex; // Global lock to control access
-pthread_cond_t condition[NUM_PHILOSOPHERS]; // condition variable for the philosopher
+pthread_mutex_t mutex; // Global lock
+pthread_cond_t condition[NUM_PHILOSOPHERS]; // Condition variables
 
+void pickup_forks(int p_num);
+void return_forks(int p_num);
+
+// Get the left and right neighbors
 int left(int i) { return (i + NUM_PHILOSOPHERS - 1) % NUM_PHILOSOPHERS; }
 int right(int i) { return (i + 1) % NUM_PHILOSOPHERS; }
 
-void *philosopher_thread(){
-	while (1) {
-		/* sleeps between 1 to 3 seconds */
-        sleep(rand() % 3 + 1);
+void *philosopher_thread(void *arg) {
+    int p_num = *(int *)arg;
+    free(arg);
 
-        pickup_forks();
-		/* sleeps between 1 to 3 seconds */
-        sleep(rand() % 3 + 1);
+    while (1) {       
+        sleep(rand() % 3 + 1); // Simulate thinking
 
-        return_forks();
-		/* exits the thread */
-		pthread_exit(0);
+        pickup_forks(p_num);
+
+        printf("Philosopher %d is eating.\n", p_num);
+        sleep(rand() % 3 + 1); // Simulate eating
+
+        return_forks(p_num);
     }
 }
 
-void pickup_forks(int p_num){
+// Function to pick up forks
+void pickup_forks(int p_num) {
     pthread_mutex_lock(&mutex);
-    state[p_num] = HUNGRY;
-    test(p_num);
-    if (state[p_num] != EATING) {
-        pthread_cond_wait(&condition[p_num], &mutex); // Wait if forks are not available
+
+    while (state[left(p_num)] == EATING || state[right(p_num)] == EATING) {
+        pthread_cond_wait(&condition[p_num], &mutex);
     }
+
+    state[p_num] = EATING;
+    printf("Philosopher %d picks up forks and starts eating.\n", p_num);
+
     pthread_mutex_unlock(&mutex);
 }
 
-void return_forks(){
 
-}
+void return_forks(int p_num) {
+    pthread_mutex_lock(&mutex);
 
-// Checks if the philosopher can eat or not
-void test(int p_num){
-    // The if condition checks if the philosopher is HUNGRY and checks if both left and right neighbors are not eating
-    // If that is true only then it allows the philosophers to eat 
-    if (state[p_num] == HUNGRY && state[left(p_num)] != EATING && state[right(p_num)] != EATING) {
-        state[p_num] = EATING;
-        printf("The philosopher %d picks up forks and starts eating.\n", p_num);
+    state[p_num] = THINKING;
+    printf("Philosopher %d has stopped eating and returns forks.\n", p_num);
+    printf("Philosopher %d is thinking.\n", p_num);
+    // Notifying neighbors that the current philosopher has stopped eating 
+    pthread_cond_signal(&condition[left(p_num)]);
+    pthread_cond_signal(&condition[right(p_num)]);
 
-        pthread_cond_signal(&condition[p_num]); // Notify waiting philosopher
-    }
+    pthread_mutex_unlock(&mutex);
 }
 
 
 int main() {
-    // pthread_t tid; /* the thread identifier */
-	// pthread_attr_t attr; /* set of thread attributes */
-	// /* set the default attributes of the thread */
-	// pthread_attr_init(&attr);
-	// /* create the thread */
-	// pthread_create(&tid, &attr, philosopher_thread, argv[1]);
-	// /* wait for the thread to exit */
-	// pthread_join(tid,NULL);
     int i;
     pthread_t philosophers[NUM_PHILOSOPHERS];
 
-    // Initialize the mutex and condition variables
+    // Initializing the mutex and condition variables
     pthread_mutex_init(&mutex, NULL);
     for (i = 0; i < NUM_PHILOSOPHERS; i++) {
         pthread_cond_init(&condition[i], NULL);
         state[i] = THINKING;
     }
 
-    // Create philosopher threads
+    // Creating the philosopher threads
     for (i = 0; i < NUM_PHILOSOPHERS; i++) {
         int *id = malloc(sizeof(int));
         *id = i;
         pthread_create(&philosophers[i], NULL, philosopher_thread, id);
+        printf("Philosopher %d is thinking.\n", *id);
     }
 
     // Join threads
