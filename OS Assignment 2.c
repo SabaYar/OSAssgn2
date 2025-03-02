4992f6fa97b3ab3b1bc3b4cf391aa1dd67690d63
Saba Yar <yars@sheridancollege.ca>
2025-03-01 19:15:04 -0500

    Added to philosopher_thread() and main()

--- OS Assignment 2.c
+++ OS Assignment 2.c
@@ -4,14 +4,29 @@
 #include <unistd.h>
 
 #define NUM_PHILOSOPHERS 5 // Number of the philosophers
-typedef enum { THINKING, HUNGRY, EATING } state_t; // State of the philosopher
+typedef enum { THINKING, EATING } state_t; // State of the philosopher
 state_t state[NUM_PHILOSOPHERS];
 
 pthread_mutex_t mutex; // Global lock to control access
 pthread_cond_t condition[NUM_PHILOSOPHERS]; // condition variable for the philosopher
 
-void *philosopher_thread(){
+void *philosopher_thread();  /* threads call this function */
+void pickup_forks();
+void return_forks();
 
+void *philosopher_thread(){
+	while (1) {
+		/* sleeps between 1 to 3 seconds */
+        sleep(rand() % 3 + 1);
+
+        pickup_forks();
+		/* sleeps between 1 to 3 seconds */
+        sleep(rand() % 3 + 1);
+
+        return_forks();
+		/* exits the thread */
+		pthread_exit(0);
+    }
 }
 
 void pickup_forks(){
@@ -28,6 +43,14 @@ void test(){
 }
 
 
-int main(){
+int main(int argc, char *argv[]){
+	pthread_t tid; /* the thread identifier */
+	pthread_attr_t attr; /* set of thread attributes */
+	/* set the default attributes of the thread */
+	pthread_attr_init(&attr);
+	/* create the thread */
+	pthread_create(&tid, &attr, philosopher_thread, argv[1]);
+	/* wait for the thread to exit */
+	pthread_join(tid,NULL);
     return 0;
 }
