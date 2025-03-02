a3d2715fb684918933ef8d9fd44d43e9b0be0058
Saba Yar <yars@sheridancollege.ca>
2025-03-02 18:45:27 -0500

    Added comments

--- OS Assignment 2.c
+++ OS Assignment 2.c
@@ -4,8 +4,8 @@
 #include <unistd.h>
 
 #define NUM_PHILOSOPHERS 5 // Number of philosophers
-typedef enum { THINKING, EATING } state_t; // States
-state_t state[NUM_PHILOSOPHERS];
+typedef enum { THINKING, EATING } state_t; // Philosopher states
+state_t state[NUM_PHILOSOPHERS]; // State of each philosopher
 
 pthread_mutex_t mutex; // Global lock
 pthread_cond_t condition[NUM_PHILOSOPHERS]; // Condition variables
@@ -17,47 +17,52 @@ void return_forks(int p_num);
 int left(int i) { return (i + NUM_PHILOSOPHERS - 1) % NUM_PHILOSOPHERS; }
 int right(int i) { return (i + 1) % NUM_PHILOSOPHERS; }
 
+// Function that each thread will loop
 void *philosopher_thread(void *arg) {
+	// Converting cmd line arg to int
     int p_num = *(int *)arg;
     free(arg);
-
-    while (1) {       
-        sleep(rand() % 3 + 1); // Simulate thinking
-
+	
+    while (1) {
+		// Wait for 1 to 3 seconds before attempting to pick up forks
+        sleep(rand() % 3 + 1); 
         pickup_forks(p_num);
-
-        printf("Philosopher %d is eating.\n", p_num);
-        sleep(rand() % 3 + 1); // Simulate eating
-
+        // Wait for 1 to 3 seconds before attempting to return forks
+        sleep(rand() % 3 + 1);
         return_forks(p_num);
     }
 }
 
 // Function to pick up forks
 void pickup_forks(int p_num) {
+	// Lock critical section
     pthread_mutex_lock(&mutex);
-
+	// Wait until neighbours have stopped eating before picking up forks
     while (state[left(p_num)] == EATING || state[right(p_num)] == EATING) {
         pthread_cond_wait(&condition[p_num], &mutex);
     }
-
+	// Change current philosopher state to EATING
     state[p_num] = EATING;
-    printf("Philosopher %d picks up forks and starts eating.\n", p_num);
-
+	// Notify others that current philosopher state has changed to EATING
+    printf("Philosopher %d picks up forks.\n", p_num);
+	printf("Philosopher %d is eating.\n", p_num);
+	// Unlock critical section
     pthread_mutex_unlock(&mutex);
 }
 
-
+// Function to return forks
 void return_forks(int p_num) {
+	// Lock critical section
     pthread_mutex_lock(&mutex);
-
+	// Change current philosopher state to THINKING
     state[p_num] = THINKING;
+	// Notify others that current philosopher state has changed to THINKING
     printf("Philosopher %d has stopped eating and returns forks.\n", p_num);
     printf("Philosopher %d is thinking.\n", p_num);
-    // Notifying neighbors that the current philosopher has stopped eating 
+    // Signal neighbors that the current philosopher has stopped eating 
     pthread_cond_signal(&condition[left(p_num)]);
     pthread_cond_signal(&condition[right(p_num)]);
-
+	// Unlock critical section
     pthread_mutex_unlock(&mutex);
 }
 
